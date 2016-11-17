2ad64521b1e35f651bc63b161b7d27e85afe2719
rapter2006 <rapter2006@ya.ru>
2016-11-17 17:15:41 +0700

    Add new commands to CommandHolder

--- command_holder.py
+++ command_holder.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from weather import Weather
-from telegram_bot import Telegram
-
+from habrahabr import Habrahabr
+from news import News
 
 class CommandHolder(object):
     def __init__(self):
-        #telegram = Telegram()
         weather = Weather()
+        habr = Habrahabr()
+        news = News()
         self.commands = {
-            'погода': weather.say_weather()
+            'погода': weather.say_weather(),
+            'хабр': habr.say_habr_news(10),
+            'новости': news.say_news(10),
         }
-        #self.telegram_commands = {
-        #    'погода': telegram.say_text(weather.get_weather())
-        #}
 
     def run_command(self, command_string, telegram=True):
         self.commands[command_string]
 
-
     def get_command_list(self):
         return self.command_list
+
+
--- test.py
+++ test.py
@@ -4,7 +4,7 @@
 # from speech import Speech
 # from command_holder import CommandHolder
 # from news import News
-from telegram_bot import Telegram
+# from telegram_bot import Telegram
 #from habrahabr import Habrahabr
 # w = Weather()
 # weather = w.get_weather()
@@ -18,4 +18,4 @@ from telegram_bot import Telegram
 # h = Habrahabr()
 # h.get_news()
 
-t = Telegram()
+# t = Telegram()
